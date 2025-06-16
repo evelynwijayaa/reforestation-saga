0d78ca2d0f7e438a7c9b5a93c4c5f2ec7809624b
Nur Fajar Sayyidul Ayyam <nur.fajarsa@gmail.com>
2025-06-16 08:01:01 +0700

    refactor: use eye blink detector vision

--- ReforestationSaga/GameView.swift
+++ ReforestationSaga/GameView.swift
@@ -9,15 +9,15 @@ import SpriteKit
 import SwiftUI
 
 struct GameView: View {
-    @StateObject private var detector = EyeBlinkDetector()
+    @StateObject private var detector = EyeBlinkDetectorVision()
     @State private var showAlert = false
     @State private var scene: GameScene?
     @State private var isMuted: Bool = false
 
     var body: some View {
         ZStack {
-            
-            CameraPreviewView(session: detector.captureSession)
+
+            CameraViewVision(eyeBlinkDetector: detector)
                 .edgesIgnoringSafeArea(.all)
             Image("background-level-1")
                 .resizable()
@@ -38,7 +38,7 @@ struct GameView: View {
                             scene?.resumeMusic()
                         }
                     } label: {
-                        Image(isMuted ? "sound-off" :"sound-on")
+                        Image(isMuted ? "sound-off" : "sound-on")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 30, height: 30)
@@ -47,10 +47,10 @@ struct GameView: View {
                 }
                 Spacer()
             }
-            
+
         }
         .onAppear {
-            detector.startCamera()
+            //            detector.startCamera()
             let newScene = GameScene(size: CGSize(width: 400, height: 800))
             newScene.scaleMode = .resizeFill
             newScene.onFailZoneHit = {
@@ -59,8 +59,8 @@ struct GameView: View {
             self.scene = newScene
         }
         // 🔥 Detect change and trigger function
-        .onChange(of: detector.predictionLabel) {
-            scene!.triggerAction(detector.predictionLabel)
+        .onChange(of: detector.blinkCount) {
+            scene?.shootNeedle()
         }
         .alert("Mission Failed!", isPresented: $showAlert) {
             Button("Try Again", role: .cancel) {}
